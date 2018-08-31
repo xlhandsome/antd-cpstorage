772df25ec2d81d477a3567a032080d7ded910f28
xulei <449341701@qq.com>
2018-08-31 16:40:22 +0800

    update sider

--- src/components/Sider.js
+++ src/components/Sider.js
@@ -21,12 +21,14 @@ class Sider extends React.Component {
   }
   mapMenu = ()=>{
     return menuConfig.map((item,index)=>{
-      return <Menu.Item key={index}>
+      return <Menu.Item key={item.path}>
               <a href={item.path}>{item.name}</a>
              </Menu.Item>
     })
   }
   render() {
+    const path = `/${window.location.hash}`;
+    console.log(path,"???")
     return (
       <div style={{height:'100%'}}>
         <Switch
@@ -42,7 +44,7 @@ class Sider extends React.Component {
           onClick={this.handleClick}
           style={{ width: '100%' }}
           defaultOpenKeys={['sub1']}
-          selectedKeys={[this.state.current]}
+          selectedKeys={[path]}
           mode="inline"
         >
           <SubMenu key="sub1" title={<span><Icon type="cloud" /><span>组件</span></span>}>
